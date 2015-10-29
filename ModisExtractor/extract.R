4c8cb7ec0825b70c80328f22a00b81fd80666772
MarconiS <s.marconi@ufl.edu>
2015-10-29 01:06:21 -0400

    Update extract.R
    added MCD12Q1 in the script

--- ModisExtractor/extract.R
+++ ModisExtractor/extract.R
@@ -1,6 +1,9 @@
 #this little script may be used every time it is useful to collect MODIS data for a wide 
 #amount of pixels from the Land Global Subset. The different products are:
 
+library(MODISTools)
+library(dplyr)
+
 # #"MCD12Q1"    LAnd Cover type product >>500m
 # "MCD12Q2"     Land Cover Dynamics >> 500m
 # "MCD43A1"     Modis Terra Aqua Albedo  
@@ -19,25 +22,38 @@
 # "MYD13Q1"     NDVI/EVI
 # "MYD15A2"     LAI and FPAR
 
-library(MODISTools)
-library(dplyr)
 
-#this case is for the Tapajos basin. The csv file was extracted from a polygon, 0.18 degrees to raster (~1590 pixels). 
-#Pixels coordinates were sampled from the produced raster. 
-Export_1590t <- read.csv("~/Documents/Ecohydrology/Export_1590t.txt", header=FALSE)
+#modis.coordinates <-"dec"
+#modis.product <-"MOD16A2"
+modis.product <-"MCD12Q1"
+
+Export_Output <- read.csv("~/Documents/Ecohydrology/Project/AmazonConceptualBalance/41.txt", header=FALSE)
 #View(Export_1590t)
-modis.data <-Export_1590t[,c(3,4)];
-#Since long and lat were inverted in my poligon to coordinates extraction, I had to invert the order of the columns
+modis.data <-Export_Output[,c(3,4)];
 modis.data <-modis.data[, c("V4","V3")]
 colnames(modis.data) <- c('lat','long')
-modis.sDate <- 2000
-modis.eDate <- 2000
-modis.coordinates <-"dec"
-modis.product <-"MOD16A2"
-modis.bands <- c("ET_1km")
-#modis.subset <-modis.subset[1,]
+#
+
+if (modis.product == "MOD16A2")
+  {modis.bands <- c("ET_1km", "ET_QC_1km")
+  #basin ~460,000 km2. MOD16A2 gives back 1km2 data: hence each subset is going to be (110)^2 cells
+  bSize <-54
+}else if(modis.product == "MCD12Q1")
+  {modis.bands <- c("Land_Cover_Type_1", "LC_Property_1", "Land_Cover_Type_1_Secondary")
+  # TODO: MCD12Q1 gives back annual data 500m2 resolution. it should be bSize <-108; max accepted is 100.
+  bSize <-100
+}else
+#for the study only MOD16A2 and MCD12Q1 accepted
+  {print(exit <-'Exit = 1')
+}
+
+dd <- modis.tempSeries <-GetDates(Product = modis.product, Lat = modis.data$lat[1], Long = modis.data$long[1])
+#by now handmade; divide the AYYYYDDD into real dates. Remember: first and last of the vector dd
+modis.sDate <- 2001
+modis.eDate <- 2009
+
 modis.data$start.date <- rep(modis.sDate, nrow(modis.data))
 modis.data$end.date <- rep(modis.eDate, nrow(modis.data))
-#the basin counts for ~480,000 km2. ModisTool works faster collecting less input pixels with wide around, than the opposite
-# the size is a 9x9, which means a 19x19 matrix will be extracted around the selected point, for each point.
-MODISSubsets(LoadDat = modis.data, Products = modis.product, Bands = modis.bands, Size = c(9,9), StartDate = TRUE)
+
+MODISSubsets(LoadDat = modis.data, Products = modis.product, Bands = modis.bands, 
+             Size = c(bSize,bSize), StartDate = TRUE)
